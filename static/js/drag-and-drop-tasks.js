62c8b96f4a4481738049edef30393b70c9ab58ba
Alexander <ipz203_tsos@student.ztu.edu.ua>
2022-05-12 13:22:13 +0300

    Improved drag and drop sorting method

--- static/js/drag-and-drop-tasks.js
+++ static/js/drag-and-drop-tasks.js
@@ -44,17 +44,27 @@ if (divsGripDraggable && container) {
         event.preventDefault();
 
         let elemDraggable = document.querySelector('div.dragging');
-        let card = event.target.closest('div.wrapper-card');
+        let elemOver = event.target.closest('div.wrapper-card');
 
-        if (card && elemDraggable !== card) {
-            let rect = card.getBoundingClientRect();
-            let next = (event.clientX - rect.left) / (rect.right - rect.left) > 0.5;
+        if (elemOver && elemDraggable !== elemOver) {
+            let position;
 
-            if (next) {
-                card.insertAdjacentElement('afterend', elemDraggable);
+            if (elemOver === elemDraggable.nextElementSibling) {
+                position = 'afterend';
+            } else if (elemOver === elemDraggable.previousElementSibling) {
+                position = 'beforebegin';
             } else {
-                card.insertAdjacentElement('beforebegin', elemDraggable);
+                let rect = elemOver.getBoundingClientRect();
+                let next = (event.clientX - rect.left) / (rect.right - rect.left) > 0.5;
+
+                if (next) {
+                    position = 'afterend';
+                } else {
+                    position = 'beforebegin';
+                }
             }
+
+            elemOver.insertAdjacentElement(position, elemDraggable);
         }
     });
 }
