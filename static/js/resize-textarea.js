4622fcb5d6284a739b7bfc471cf7a1f4df834027
Alexander <ipz203_tsos@student.ztu.edu.ua>
2022-05-19 09:54:52 +0300

    Updated textarea resize method

--- static/js/resize-textarea.js
+++ static/js/resize-textarea.js
@@ -2,9 +2,9 @@ let resizeTextareas = document.querySelectorAll('textarea.resize-none');
 
 resizeTextareas.forEach(textarea => {
     textarea.addEventListener('input', function() {
+        this.removeAttribute('rows');
+
         this.style.height = 'auto';
         this.style.height = this.scrollHeight + 'px';
-
-        this.removeAttribute('rows');
     });
 });
